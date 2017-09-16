4c2eaf92e1a5a3552c7911230ccdbc455b7059b4
Saria Islam <saraaaay@gmail.com>
2017-09-16 14:56:34 -0700

    Add getCollection function to Fixtures service, inject Fixtures into CollectionCtrl

--- app/scripts/services/Fixtures.js
+++ app/scripts/services/Fixtures.js
@@ -36,6 +36,16 @@
            return albumPicasso;
          };
 
+         Fixtures.getCollection = function (numberOfAlbums) {
+           var albums = [];
+
+           for (var i = 0; i < numberOfAlbums; i++) {
+             albums.push(angular.copy(albumPicasso));
+           }
+
+           return albums;
+         };
+
          return Fixtures;
      }
  
